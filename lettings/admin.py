87a8ea488ea1484fb9d77f2c8b24729860869966
Clement Jeulin <clement93jeulin@gmail.com>
2025-08-07 23:49:16 +0200

    Migrate data to the new structure

--- lettings/admin.py
+++ lettings/admin.py
@@ -3,6 +3,5 @@ from .models import Letting
 from .models import Address
 
 # Register your models here.
-
 admin.site.register(Letting)
 admin.site.register(Address)
---
+++ lettings/migrations/0002_copy_data_from_old_models.py
@@ -0,0 +1,35 @@
+from django.db import migrations
+
+def copy_address_and_letting(apps, schema_editor):
+    OldAddress = apps.get_model('oc_lettings_site', 'Address')
+    OldLetting = apps.get_model('oc_lettings_site', 'Letting')
+    NewAddress = apps.get_model('lettings', 'Address')
+    NewLetting = apps.get_model('lettings', 'Letting')
+
+    for old_address in OldAddress.objects.all():
+        new_address = NewAddress.objects.create(
+            number=old_address.number,
+            street=old_address.street,
+            city=old_address.city,
+            state=old_address.state,
+            zip_code=old_address.zip_code,
+            country_iso_code=old_address.country_iso_code,
+        )
+        old_address.new = new_address  # lien temporaire
+
+    for old_letting in OldLetting.objects.all():
+        # on retrouve la nouvelle adresse correspondante
+        address = NewAddress.objects.get(
+            number=old_letting.address.number,
+            street=old_letting.address.street
+        )
+        NewLetting.objects.create(title=old_letting.title, address=address)
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('lettings', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.RunPython(copy_address_and_letting),
+    ]
